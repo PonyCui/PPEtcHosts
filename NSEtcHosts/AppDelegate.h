1e1b8ff0049948916ebef6791d426d89033c69e0
PonyCui <cuiminghui@yy.com>
2015-04-23 10:34:11 +0800

    Initial Commit

---
+++ NSEtcHosts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSEtcHosts
+//
+//  Created by 崔 明辉 on 15/4/23.
+//  Copyright (c) 2015年 PonyCui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
